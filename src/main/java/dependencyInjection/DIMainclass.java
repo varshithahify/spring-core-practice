6bb9debe104f9a04eb387edf3510e31398056bf7
varshithahify <varshithavn26@gmail.com>
2026-07-05 16:50:55 +0530

    added types of Dependency injection example programs

--- src/main/java/dependencyInjection/DIMainclass.java
+++ src/main/java/dependencyInjection/DIMainclass.java
@@ -1,5 +1,70 @@
 package dependencyInjection;
 
-public class DIMainclass {
+//using constructor injection
+//	class Pen{
+//		void write() {
+//			System.out.println("writing");
+//		}
+//	}
+//	class Student{
+//		Pen pen;
+//		Student(Pen pen){
+//			this.pen=pen;
+//		}
+//		void study() {
+//			pen.write();
+//		}
+//	}
+//public class DIMainclass {
+//	public static void main(String[] args) {
+//	Pen p=new Pen();
+//	Student s=new Student(p);
+//	s.study();
+//}}
+//------------------------------------------------
+//using setter injection
+//class Pen{
+//	void write() {
+//		System.out.println("writing");
+//	}
+//}
+//class Student{
+//	Pen pen;
+//	void setPen(Pen pen){
+//		this.pen=pen;
+//	}
+//	void study() {
+//		pen.write();
+////		System.out.println("wr1");
+//	}
+//}
+//public class DIMainclass {
+//public static void main(String[] args) {
+//Pen p=new Pen();
+//Student s=new Student();
+//s.setPen(p);
+//s.study();
+//}}
 
+//------------------------------------------------------------
+//using manual field injection
+class Pen{
+void write() {
+	System.out.println("writing");
 }
+}
+class Student{
+   Pen pen;
+    
+   void study() {
+	pen.write();
+	System.out.println("studying");
+}
+}
+public class DIMainclass {
+public static void main(String[] args) {
+Student s=new Student();
+s.pen=new Pen();
+s.study();
+
+}}
