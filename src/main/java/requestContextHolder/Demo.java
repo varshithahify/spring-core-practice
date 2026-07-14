f258420b680c695eec8a28899889ea8b29cec355
varshithahify <varshithavn26@gmail.com>
2026-07-14 21:09:15 +0530

    added example program on RequestContextHolder

---
+++ src/main/java/requestContextHolder/Demo.java
@@ -0,0 +1,12 @@
+package requestContextHolder;
+
+import org.springframework.web.context.request.RequestContextHolder;
+
+public class Demo {
+	 public static void main(String[] args) {
+		 System.out.println(RequestContextHolder.getRequestAttributes());
+	    }
+}
+
+	                
+	                        
