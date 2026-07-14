2aba9b44a0176dc9d5ee080af3fdc94ea7db226c
varshithahify <varshithavn26@gmail.com>
2026-07-14 21:05:37 +0530

    added program on dispatcher servlet context

---
+++ src/main/java/dispatcherServletContext/DispatcherServletContext.java
@@ -0,0 +1,13 @@
+package dispatcherServletContext;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class DispatcherServletContext {
+	public static void main(String[] args) {
+		DispatcherServlet servlet = new DispatcherServlet();
+		System.out.println(servlet.getClass().getSimpleName());         
+	}
+	}
+        
+                
+    
